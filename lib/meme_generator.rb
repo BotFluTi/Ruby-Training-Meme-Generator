25649bbc950739ee766d43fbfdd3071548961f02
BotFluTi <162293866+BotFluTi@users.noreply.github.com>
2026-07-11 12:13:20 +0300

    Initialize project and add initial model tests

---
+++ lib/meme_generator.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'mini_magick'
+
+# rubocop:disable Metrics/MethodLength
+class MemeGenerator
+  def self.generate(file_path, text)
+    generated_file_path = file_path.sub(/original_/, 'generated_')
+    image = MiniMagick::Image.open(file_path)
+    image.combine_options do |c|
+      c.gravity 'center'
+      c.fill 'black'
+      c.undercolor 'white'
+      c.font '/System/Library/Fonts/Helvetica.ttc'
+      c.pointsize 20
+      c.draw %(text 0,50 "#{text}")
+    end
+    image.write(generated_file_path)
+
+    generated_file_path
+  end
+end
+# rubocop:enable Metrics/MethodLength
---
+++ spec/lib/meme_data_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require './lib/meme_data'
+
+RSpec.describe MemeData do
+  describe '#initialize' do
+    subject(:meme_data) do
+      described_class.new('https://example.com/image.jpg',
+                          'Hello world')
+    end
+
+    it 'stores the image URL' do
+      expect(meme_data.image_url).to eq('https://example.com/image.jpg')
+    end
+
+    it 'stores the text' do
+      expect(meme_data.text).to eq('Hello world')
+    end
+  end
+end
