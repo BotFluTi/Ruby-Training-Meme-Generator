e11a581c6839f0118cddda835db76bb0b9514589
BotFluTi <162293866+BotFluTi@users.noreply.github.com>
2026-07-11 13:58:50 +0300

    more tests

--- lib/meme_controller.rb
+++ lib/meme_controller.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
-require_relative 'response'
+require './lib/response'
+require './lib/json_parser'
 
 class MemeController
-  def execute(_body)
+  def execute(body)
+    meme_data = JsonParser.new.parse(body)
+
+    return Response.new(message: 'Empty body') if meme_data.nil?
+
+    return Response.new(message: 'Check URL field') if meme_data.image_url.nil? || meme_data.image_url.empty?
+
+    return Response.new(message: 'Check text field') if meme_data.text.nil? || meme_data.text.empty?
+
     Response.new(redirect_url: 'generated.jpg')
   end
 end
--- spec/lib/api_spec.rb
+++ spec/lib/api_spec.rb
@@ -6,11 +6,15 @@ require 'rack/test'
 require 'rspec'
 require_relative '../../api'
 
-RSpec.describe 'Meme API' do
+RSpec.describe 'MemeData' do
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
+  end
+
+  before do
+    post '/memes', body, { 'CONTENT_TYPE' => 'application/json' }
   end
 
   context 'when the request is correct' do
@@ -20,6 +24,45 @@ RSpec.describe 'Meme API' do
       post '/memes', body, { 'CONTENT_TYPE' => 'application/json' }
 
       expect(last_response.status).to eq(303)
+    end
+  end
+
+  context 'when the request is missing the image URL' do
+    let(:body) { File.read('spec/fixtures/no_link_test.json') }
+    let(:response_body) { JSON.parse(last_response.body) }
+
+    it 'returns status code 400' do
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'returns an error message' do
+      expect(response_body['message']).to include('Check URL field')
+    end
+  end
+
+  context 'when the request is missing text' do
+    let(:body) { File.read('spec/fixtures/no_text_test.json') }
+    let(:response_body) { JSON.parse(last_response.body) }
+
+    it 'returns status code 400' do
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'returns an error message' do
+      expect(response_body['message']).to include('Check text field')
+    end
+  end
+
+  context 'when the request body is empty' do
+    let(:body) { File.read('spec/fixtures/empty_json_test.json') }
+    let(:response_body) { JSON.parse(last_response.body) }
+
+    it 'returns status code 400' do
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'returns an error message' do
+      expect(response_body['message']).to include('Empty body')
     end
   end
 end
